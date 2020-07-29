faeeb52e1071d8c052eabea4a424238b23992ee5
blakeaham <blakeaham@gmail.com>
2020-07-29 18:02:50 -0500

    exercise 19
    completed exercise 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -25,7 +25,80 @@ function paintToCanvas() {
 
     setInterval(() =>{
         ctx.drawImage(video, 0, 0, width, height);
-    }, 22);
+        let pixels = ctx.getImageData(0, 0, width, height);
+       
+        pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.2;
+        pixels = greenScreen(pixels);
+        ctx.putImageData(pixels, 0, 0);
+    }, 20);
 }
 
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Pic of U " />`;
+    strip.insertBefore(link, strip.firstChild);
+
+}
+
+function redEffect(pixels) {
+    for(let i=0; i < pixels.data.length; i+= 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 90; //RED
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; //GREEN
+        pixels.data[i + 2] = pixels.data[i + 2] - 50; //BLUE
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+      })
+
+    let pixInterval = levels.jumpp;
+    for(let i=0; i < pixels.data.length; i+= 4) {
+        pixels.data[i - levels.rshift] = pixels.data[i + 0]; //RED
+        pixels.data[i - levels.gshift + 1] = pixels.data[i + 1]; //GREEN
+        pixels.data[i - levels.bshift + 2] = pixels.data[i + 2]; //BLUE
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+  
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+  
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+      }
+    }
+    return pixels;
+};
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
